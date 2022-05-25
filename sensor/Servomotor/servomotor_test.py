4f65b5906465072623e9707e06709151efec27bc
Toshiki-Keio <ftu-0770.txs@keio.jp>
2022-05-25 15:37:25 +0900

    add Motor program

---
+++ sensor/Servomotor/servomotor_test.py
@@ -0,0 +1,25 @@
+import servomotor
+import RPi.GPIO as GPIO
+import time 
+
+GPIO.setwarnings(False)
+servomotor = servomotor.servomotor(25)
+
+
+try:
+    #while True:
+    print("servomotor run")
+    servomotor.servo_angle(-90)
+    time.sleep(1)
+    servomotor.servo_angle(0)#CLOSE
+    time.sleep(1)
+    servomotor.servo_angle(90)
+    time.sleep(1)
+    #servomotor.servo_angle(0)#OPEN
+#     time.sleep(0.3)
+    servomotor.stop()
+    
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    #servomotor.servo_angle(0) 
+    servomotor.stop()
