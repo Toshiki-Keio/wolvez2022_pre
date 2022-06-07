bde34c0a9f87ba1b72d7266a669816671af1abcf
Toshiki-Keio <ftu-0770.txs@keio.jp>
2022-06-07 21:55:56 +0900

    add cameratest

---
+++ sensor/camera/cameratest.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+from cv2 import aruco
+import sys
+
+def main(args):
+    """
+    引数0→写真
+    引数1→動画
+    """
+    cap = cv2.VideoCapture(0)
+    if args == "0":
+        ret,img = cap.read()
+        cv2.imwrite('test.jpg', img)
+        
+    else:
+        while True:
+            ret, img = cap.read()
+            cv2.imshow('cameratest', img)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    args = sys.argv
+    main(args[1])
