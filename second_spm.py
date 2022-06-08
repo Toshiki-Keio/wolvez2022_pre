3ab0e14473cf9b58479785a003ec0e9b8ecfe3cf
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-06-08 16:07:27 +0900

    regular backup

--- second_spm.py
+++ second_spm.py
@@ -4,13 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
 
-train_X=np.random.rand(10,5)# 特徴画像の数＊特徴量ベクトル＊学習画像の数　を想定
+train_X=np.random.rand(10,5)
+# 特徴画像の数＊特徴量ベクトル＊学習画像の数　を想定
+print(train_X.shape)
 train_y=np.random.rand(10)
+#test_X=np.random.rand(10,5).reshape(-1,1)
 
 model=Lasso()
 model.fit(train_X,train_y)
 
+#test_y=model.predict(test_X)
+
+
 """
-・Xを3次元にできない問題
+・Xを1次元にも3次元にもできない問題
 ・今後何らかの支障になるかもなと思い、できればここをロバストにしてみたいと思うところです。
 """
