d27b2f8d2e4406d54b1988e8b4b96fa16266c220
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-06-09 18:54:26 +0900

    ２段目v.1.0完成！あとはデータ入れるだけです

--- second_spm.py
+++ second_spm.py
@@ -3,18 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
+import os
 
-train_X=np.random.rand(10,5)
+path=os.getcwd()
+train_X=np.load(path+"/second_input_data/2022-06-09_18-44-40.npz")["array_1"]
+
 # 特徴画像の数＊特徴量ベクトル＊学習画像の数　を想定
 print(train_X.shape)
-train_y=np.random.rand(10)
-#test_X=np.random.rand(10,5).reshape(-1,1)
-
+train_y=np.zeros((train_X.shape[0],1))
+train_y[-5:]=1
 model=Lasso()
 model.fit(train_X,train_y)
-
-#test_y=model.predict(test_X)
-
+test_X=train_X
+test_y=model.predict(test_X)
+print(model.score(test_X,test_y))
 
 """
 ・Xを1次元にも3次元にもできない問題
