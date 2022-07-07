1803ea586a5c89be362688caceb7dea172fb7a7e
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-07-07 23:20:11 +0900

    gensen-simasita

--- spm/c_spm2/cb_spm2_learn/cbzz_memo.py
+++ spm/c_spm2/cb_spm2_learn/cbzz_memo.py
@@ -67,7 +67,7 @@ class Learn():
         self.fps = fps
         self.data_list_all_win=data_list_all_win
         self.label_list_all_win=label_list_all_win
-        print(data_list_all_win.shape)#(win,pic_num,feature)=(6,886,30)
+        # print(data_list_all_win.shape)#(win,pic_num,feature)=(6,886,30)
         if stack_info==None:
             self.stack_appear = stack_appear
             self.stack_disappear = stack_disappear
@@ -76,7 +76,7 @@ class Learn():
             self.stack_info=np.zeros((self.data_list_all_win.shape[0],2))
             self.stack_info[:,0]=int(self.stack_appear_frame)
             self.stack_info[:,1]=int(self.stack_disappear_frame)
-            pprint(self.stack_info)
+            # pprint(self.stack_info)
         else:
             self.stack_info=stack_info*self.fps
             pass
@@ -99,9 +99,9 @@ class Learn():
             self.scaler_master[win_no]=self.standardization_master[win_no].fit(train_X)
             train_X=self.scaler_master[win_no].transform(train_X)
             train_y = np.zeros((train_X.shape[0], 1))
-            print(self.stack_info[win_no][0])
+            # print(self.stack_info[win_no][0])
             train_y[int(self.stack_info[win_no][0]):int(self.stack_info[win_no][1])] = 100
-            print(train_X.shape, train_y.shape)
+            # print(train_X.shape, train_y.shape)
             self.model_master[win_no].fit(train_X, train_y)
             pass
         pass
@@ -111,10 +111,11 @@ class Learn():
 
 
 class Evaluate():
-    def __init__(self,model_master,test_data_list_all_win,scaler_master,test_code):
+    def __init__(self,model_master,test_data_list_all_win,scaler_master,train_code,test_code):
         self.model_master=model_master
         self.test_data_list_all_win=test_data_list_all_win
         self.scaler_master=scaler_master
+        self.train_code=train_code
         self.test_code=test_code
         if len(self.model_master)!=len(self.test_data_list_all_win):
             print("学習済みモデルのウィンドウ数と、テストデータのウィンドウ数が一致しません")
@@ -124,17 +125,17 @@ class Evaluate():
 
 
     def test(self):
-        print(self.test_data_list_all_win)
+        # print(self.test_data_list_all_win)
         self.score_master=[]
         for win_no in range(np.array(self.test_data_list_all_win).shape[0]):
             self.score_master.append([])
         for test_no in range(np.array(self.test_data_list_all_win).shape[1]):
             for win_no, win in enumerate(self.test_data_list_all_win):
                 test_X = win[test_no]
-                print(f"test_X win_no\n: {win_no}",test_X)
+                # print(f"test_X win_no\n: {win_no}",test_X)
                 test_X=self.scaler_master[win_no].transform(test_X.reshape(1, -1))
                 score = self.model_master[win_no].predict(test_X.reshape(1, -1))
-                print(score)
+                # print(score)
                 self.score_master[win_no].append(score)
                 pass
         # pprint(self.score_master[0])
@@ -145,7 +146,7 @@ class Evaluate():
         plt.xlabel("time")
         plt.ylabel("degree of risk")
         plt.legend()
-        plt.savefig(f"c_spm2/cc_spm2_after/cca_output_of_spm2/ccai{self.test_code}_L-bcci_P-bcc{self.test_code}.png")
+        plt.savefig(f"c_spm2/cc_spm2_after/cca_output_of_spm2/cca{self.train_code}{self.test_code}_L-bcc{self.train_code}_P-bcc{self.test_code}.png")
         plt.cla()
         # plt.show()
 
@@ -153,45 +154,56 @@ class Evaluate():
 
 
 # wolvez2022/spmで実行してください
-spm_path = os.getcwd()
-train_files = sorted(glob.glob(spm_path+"/b_spm1/b-data/bcca_secondinput/bcci/*"))
+# train_codes=['a','b','c','d','e','f','g','h','i']
+# test_codes=['a','b','c','d','e','f','g','h','i']
+# stack_starts=[0.,0.,9.,20.,11.,4.,35.,58.,10.,]#bそもそもスタートがスタック,g白砂利道,hスタック以外の原因で修理・パソコン映り込みも,iスタック以外の原因で停止は学習データ自体が悪い
+# stack_ends=[4.,5.,16.,24.,13.,6.,36.,120.,11.,]
+train_codes=['a','c','d','e','f']
+test_codes=['a','c','d','e','f']
+stack_starts=[0.,9.,20.,11.,4.]#bそもそもスタートがスタック,g白砂利道,hスタック以外の原因で修理・パソコン映り込みも,iスタック以外の原因で停止は学習データ自体が悪い
+stack_ends=[4.,16.,24.,13.,6.]
 
-seq1=Open_npz(train_files)
-data_list_all_win,label_list_all_win=seq1.get_data()
+for train_code in train_codes:
+    print("train data mov code : ",train_code)
+    spm_path = os.getcwd()
+    train_files = sorted(glob.glob(spm_path+f"/b_spm1/b-data/bcca_secondinput/bcc{train_code}/*"))
 
-stack_info=np.array([[12., 18.],
-       [12., 18.],
-       [12., 18.],
-       [12., 18.],
-       [12., 18.],
-       [12, 18.]])
-"""
-「stackした」と学習させるフレームの指定方法
-1. 全ウィンドウで一斉にラベリングする場合
-    Learnの引数でstack_appearおよびstack_disappearを[s]で指定する。
-2. ウィンドウごとに個別にラベリングする場合
-stack_info=np.array(
-    [
-        [win_1_stack_start,win_1_stack_end],
-        [win_2_stack_start,win_2_stack_end],
-        ...
-        [win_6_stack_start,win_6_stack_end],
-    ]
-)
-t[s]で入力すること。
-"""
-seq2=Learn(data_list_all_win,label_list_all_win,fps=30,stack_appear=5,stack_disappear=6,stack_info=None)
-#seq2=Learn(data_list_all_win,label_list_all_win,fps=30,stack_info=stack_info)
-model_master,label_list_all_win,scaler_master=seq2.get_data()
+    seq1=Open_npz(train_files)
+    data_list_all_win,label_list_all_win=seq1.get_data()
 
-spm_path = os.getcwd()
-test_codes=['a','b','c','d','e','f','g','h','i']
-for test_code in test_codes:
-    test_files = sorted(glob.glob(spm_path+f"/b_spm1/b-data/bcca_secondinput/bcc{test_code}/*"))
+    """
+    stack_info=np.array([[12., 18.],
+        [12., 18.],
+        [12., 18.],
+        [12., 18.],
+        [12., 18.],
+        [12, 18.]])
+    「stackした」と学習させるフレームの指定方法
+    1. 全ウィンドウで一斉にラベリングする場合
+        Learnの引数でstack_appearおよびstack_disappearを[s]で指定する。
+    2. ウィンドウごとに個別にラベリングする場合
+    stack_info=np.array(
+        [
+            [win_1_stack_start,win_1_stack_end],
+            [win_2_stack_start,win_2_stack_end],
+            ...
+            [win_6_stack_start,win_6_stack_end],
+        ]
+    )
+    t[s]で入力すること。
+    """
+    seq2=Learn(data_list_all_win,label_list_all_win,fps=30,stack_appear=5,stack_disappear=6,stack_info=None)
+    #seq2=Learn(data_list_all_win,label_list_all_win,fps=30,stack_info=stack_info)
+    model_master,label_list_all_win,scaler_master=seq2.get_data()
 
-    seq3=Open_npz(test_files)
-    test_data_list_all_win,test_label_list_all_win=seq3.get_data()
+    spm_path = os.getcwd()
+    for test_code in test_codes:
+        print('test data mov code : ',test_code)
+        test_files = sorted(glob.glob(spm_path+f"/b_spm1/b-data/bcca_secondinput/bcc{test_code}/*"))
 
-    seq4=Evaluate(model_master,test_data_list_all_win,scaler_master,test_code)
-    del seq3
-    del seq4
+        seq3=Open_npz(test_files)
+        test_data_list_all_win,test_label_list_all_win=seq3.get_data()
+
+        seq4=Evaluate(model_master,test_data_list_all_win,scaler_master,train_code,test_code)
+        del seq3
+        del seq4
