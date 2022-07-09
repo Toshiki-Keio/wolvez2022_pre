b30ad72145896a287ea89f335bb5c42287a0ce8f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-07-09 19:42:19 +0900

    ytpc2019a

--- spm/c_spm2/cb_spm2_learn/cbzy_memo.py
+++ spm/c_spm2/cb_spm2_learn/cbzy_memo.py
@@ -1,8 +1,4 @@
 import os
-all_path="/Users/hayashidekazuyuki/Desktop/Git_Win_Air/wolvez2022/spm/b_spm1/b-data/bczz_h_param/psize_('005', '005')-ncom_001-tcoef_001-mxiter_001.npz"
-filename=os.path.basename(all_path)
-print(filename)
-patch=int(filename[8:11])
-ncom=int(filename[26:29])
-tcoef=int(filename[36:39])
-print(tcoef)
+os.system('git add .')
+os.system('git commit -m "ytpc2019a"')
+os.system('git push origin spm')
