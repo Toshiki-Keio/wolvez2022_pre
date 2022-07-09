a2fae115c741daf23616fe337c2c1ee89d07d8bc
kumityo <takafumi-fujii0826@keio.jp>
2022-07-09 18:23:03 +0900

    from ytnpc2021e

--- planning/test_planning2.py
+++ planning/test_planning2.py
@@ -19,11 +19,11 @@ import numpy as np
 
 
 # ゴール方向の角度から行く方向を決定する関数
-def decide_direction(direction_goal_deg):
-    if direction_goal_deg >= 20:
+def decide_direction(phi):
+    if phi >= 20:
         direction_goal = 2
         print("ゴール方向："+str(direction_goal)+" -> 右に曲がりたい")
-    elif direction_goal_deg > -20 and direction_goal_deg < 20:
+    elif phi > -20 and phi < 20:
         direction_goal = 1
         print("ゴール方向："+str(direction_goal)+" -> 直進したい")
     else:
@@ -43,59 +43,50 @@ def decide_behavior(direction):
         print("90度回転する")
 
 # それぞれの方向に対して実際に行う動作を決める関数
-
-def decide_behavior_raspi(direction,MotorR,MotorL):
-    if direction == 0:
-        print("左に"+str(digree_cansatfront2goal)+"[deg] 曲がる")
-        MotorR.back(70)
-        MotorL.go(70)
-        time.sleep(1)
+def decide_behavior_raspi(direction_real, target_azimuth, phi, MotorR, MotorL, bno055):
+    bno055.bnoread()
+    bno055.ex = round(bno055.ex,3)
+    if direction_real == 0:
+        print("左に"+str(phi)+"[deg] 曲がる")
+        while bno055.ex < target_azimuth - 5 or bno055.ex > target_azimuth + 5:
+            MotorR.back(70)
+            MotorL.go(70)
+            bno055.bnoread()
+            bno055.ex = round(bno055.ex,3)       
         MotorR.stop()
         MotorL.stop()
-        
-        # 現在の方位角を取得
-#         bno055.bnoread()
-#         bno055.ex=round(bno055.ex,3)
-#         digree_north2cansatfront_now = bno055.ex
-#         digree_cansatfront2goal_now = digree_north2goal - digree_north2cansatfront_now
-# 
-#         while True:
-#             MotorR.go(70)
-#             MotorL.back(70)
-#             if digree_cansatfront2goal-5 <= digree_cansatfront2goal_now or digree_cansatfront2goal_now <= digree_cansatfront2goal + 5:
-#                 MotorR.stop()
-#                 MotorL.stop()
-#                 break
-
-    elif direction == 1:
+    elif direction_real == 1:
         print("直進する")
-        MotorR.go(70)
-        MotorL.go(70)
-        time.sleep(1)
+        bno055.bnoread()
+        bno055.ey=round(bno055.ey,3)
+        current_y = bno055.ey
+        while bno055.ey < current_y + 5 - 0.1 or bno055.ey > current_y + 5 + 0.1:
+            MotorR.go(70)
+            MotorL.go(70)
+            bno055.bnoread()
+            bno055.ey=round(bno055.ey,3)
         MotorR.stop()
         MotorL.stop()
-    elif direction == 2:
-        print("右に"+str(digree_cansatfront2goal)+"[deg] 曲がる")
-
-        MotorR.go(70)
-        MotorL.back(70)
-        time.sleep(1)
+    elif direction_real == 2:
+        print("右に"+str(phi)+"[deg] 曲がる")
+        while bno055.ex < target_azimuth - 5 or bno055.ex > target_azimuth + 5:
+            MotorR.go(70)
+            MotorL.back(70)
+            bno055.bnoread()
+            bno055.ex = round(bno055.ex,3)       
         MotorR.stop()
         MotorL.stop()
-
-    elif direction == 3:
+    elif direction_real == 3:
         print("90度回転する")
-        MotorR.back(70)
-        MotorL.go(70)
-        time.sleep(1)
+        current_azimuth = bno055.ex
+        while bno055.ex < current_azimuth + 90 - 5 or bno055.ex > current_azimuth + 90 + 5:
+            MotorR.back(70)
+            MotorL.go(70)
+            bno055.bnoread()
+            bno055.ex = round(bno055.ex,3)   
         MotorR.stop()
         MotorL.stop()
         
-
-
-
-
-
 
 # オブジェクトの生成
 
@@ -123,22 +114,22 @@ while count < 2:
     print(datalog)
     gps_dictionary = gps.vincenty_inverse(gps.Lat,gps.Lat,goal_position[0],goal_position[1])
     # print(gps_dictionary)
-    digree_north2goal = gps_dictionary["azimuth1"]
+    theta_goal = gps_dictionary["azimuth1"]
 
     # 加速度センサからCansatがどの方位角を向いているかを計測
     bno055.bnoread()
     bno055.ex=round(bno055.ex,3)
-    digree_north2cansatfront = bno055.ex
-    print("current position[deg]: "+str(digree_north2cansatfront))
+    theta_cansat = bno055.ex
+    print("current cansat azimuth[deg]: "+str(theta_cansat))
 
     # ゴールの方位角とCansatの前方方位角から，Cansatとゴールの相対角度を算出
-    digree_cansatfront2goal = digree_north2goal - digree_north2cansatfront
-    print("Cansatとゴールの相対角度[deg]: "+str(digree_cansatfront2goal))
+    phi = theta_goal - theta_cansat
+    print("Cansatとゴールの相対角度[deg]: "+str(phi))
     # direction_goal_deg = np.random.randint(-60,60)  #ゴール方向の角度を取得（後でGPSから値が取れるようにする）
 
     
     # ゴール方向の角度から左・中央・右のどの方向に行くかを算出
-    direction_goal = decide_direction(digree_cansatfront2goal)  #角度から左・前・右のどの方向に進むべきかを取得
+    direction_goal = decide_direction(phi)  #角度から左・前・右のどの方向に進むべきかを取得
     direction_real = direction_goal
 
 
@@ -157,12 +148,12 @@ while count < 2:
     if np.amin(lower_risk) >= threshold_risk:
         print("前方に安全なルートはありません。90度回転して新たな経路を探索します。")
         direction_real = 3
-        decide_behavior_raspi(direction_real,MotorR,MotorL)
+        decide_behavior_raspi(direction_real, theta_goal, phi, MotorR, MotorL, bno055)
 
     else:
         if lower_risk[direction_goal] <= threshold_risk:   #ゴール方向の危険度が閾値以下の場合
 #             decide_behavior(direction_real)   # その方向に進む
-            decide_behavior_raspi(direction_real,MotorR,MotorL)
+            decide_behavior_raspi(direction_real, theta_goal, phi, MotorR, MotorL, bno055)
         else:
             print("ゴール方向が安全ではありません。別ルートを探索します。")
             if direction_goal == 0:
@@ -171,7 +162,7 @@ while count < 2:
                 else:
                     direction_real = 2
 #                 decide_behavior(direction_real)
-                decide_behavior_raspi(direction_real,MotorR,MotorL)
+                decide_behavior_raspi(direction_real, theta_goal, phi, MotorR, MotorL, bno055)
                 # decide_behavior_raspi(direction_real,MotorR,MotorL)
             elif direction_goal == 1:
                 if lower_risk[0] <= lower_risk[2]:
@@ -180,7 +171,7 @@ while count < 2:
                     direction_real = 2
                 direction_real = 0
 #                 decide_behavior(direction_real)
-                decide_behavior_raspi(direction_real,MotorR,MotorL)
+                decide_behavior_raspi(direction_real, theta_goal, phi, MotorR, MotorL, bno055)
                 # decide_behavior_raspi(direction_real,MotorR,MotorL)
             elif direction_goal == 2:
                 if lower_risk[0] <= lower_risk[1]:
@@ -188,6 +179,6 @@ while count < 2:
                 else:
                     direction_real = 1
                 # decide_behavior(direction_real)
-                decide_behavior_raspi(direction_real,MotorR,MotorL)
+                decide_behavior_raspi(direction_real, theta_goal, phi, MotorR, MotorL, bno055)
 
 
